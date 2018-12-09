0a72883b84da7e753ab82323f35168b96ded8d95
Alexey Aleev <aleevas@gmail.com>
2018-12-09 12:18:37 +0200

    Working with PHPSpec lib

---
+++ src/RomanNumeralConverter.php
@@ -0,0 +1,23 @@
+<?php
+
+class RomanNumeralConverter
+{
+    public function convert($number)
+    {
+        $roman = '';
+
+        while ($number >= 10) {
+            $roman .= 'X';
+            $number-= 10;
+        }
+
+        if ($number >= 5) {
+            $roman .= 'V';
+            $number-= 5;
+        }
+
+        $roman .= str_repeat('I', $number);
+
+       return $roman;
+    }
+}
